64b2348eefc2f535860737bfca33525dfbaf2cd6
arifkhanever <mdshafiqul0404@gmail.com>
2026-06-23 13:36:24 +0600

    ticket post and fetch api added and tested successfully

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const dns = require("node:dns");
-dns.setServers(['8.8.8.8', '8.8.4.4', "1.1.1.1"]); 
+dns.setServers(['8.8.8.8', '8.8.4.4', "1.1.1.1"]);
 
 const express = require('express');
 const cors = require('cors');
@@ -12,8 +12,8 @@ const port = process.env.PORT || 5000;
 // ----------------- Middleware -----------------
 app.use(cors({
     origin: [
-        "http://localhost:3000", 
-        "https://your-frontend-domain.com" 
+        "http://localhost:3000",
+        "https://ticket-flow-online.vercel.app"
     ],
     credentials: true
 }));
@@ -39,12 +39,11 @@ let bookingCollection;
 async function connectDB() {
     try {
         await client.connect();
-        db = client.db("ticketflow");
-        
+        db = client.db("TicketFlow");
         ticketCollection = db.collection('tickets');
         vendorCollection = db.collection('vendors');
         usersCollection = db.collection('users');
-        sessionCollection = db.collection('sessions'); 
+        sessionCollection = db.collection('sessions');
         bookingCollection = db.collection('bookings');
 
         await client.db("admin").command({ ping: 1 });
@@ -57,26 +56,26 @@ async function connectDB() {
 connectDB();
 
 const verifyDbReady = (req, res, next) => {
-    if (!ticketCollection || !vendorCollection) {
+    if (!usersCollection || !ticketCollection) {
         return res.status(503).json({ error: "Database is initializing, please try again in a moment." });
     }
     next();
 };
 
 
-const verifyToken = async(req, res, next)=>{
+const verifyToken = async (req, res, next) => {
     const authHeader = req.headers?.authorization;
-    if(!authHeader){
-        return res.status(401).send({message: "unauthorized access"})
+    if (!authHeader) {
+        return res.status(401).send({ message: "unauthorized access" })
     }
 
     const token = authHeader.split(" ")[1]
 
-    if(!token) {
-        return res.status(401).send({message: "unauthorized token"})
+    if (!token) {
+        return res.status(401).send({ message: "unauthorized token" })
     }
 
-    const query = {token: token}
+    const query = { token: token }
     const session = await sessionCollection.findOne(query)
     const userId = session.userId
 
@@ -90,28 +89,56 @@ const verifyToken = async(req, res, next)=>{
     next()
 }
 
-const verifyUser = async(req, res, next)=> {
-    if(req.user?.role !== "user"){
-        return res.status(403).send({message: "forbidden access"})
+const verifyUser = async (req, res, next) => {
+    if (req.user?.role !== "user") {
+        return res.status(403).send({ message: "forbidden access" })
     }
     next();
 }
 
 
-const verifyVendor = async(req, res, next)=> {
-    if(req.user?.role !== "vendor"){
-        return res.status(403).send({message: "forbidden access"})
+const verifyVendor = async (req, res, next) => {
+    if (req.user?.role !== "vendor") {
+        return res.status(403).send({ message: "forbidden access" })
     }
     next();
 }
 
 
-const verifyAdmin = async(req, res, next)=> {
-    if(req.user?.role !== "admin"){
-        return res.status(403).send({message: "forbidden access"})
+const verifyAdmin = async (req, res, next) => {
+    if (req.user?.role !== "admin") {
+        return res.status(403).send({ message: "forbidden access" })
     }
     next();
 }
+
+// Root Route 
+app.get('/', verifyDbReady, async (req, res) => {
+    res.send("Tickets are hovering on the horizon. Go grab them")
+})
+
+// Creating Ticket
+app.post('/api/tickets', verifyDbReady, async (req, res) => {
+    try {
+        const ticket = { ...req.body, createdAt: new Date() }
+        const result = await ticketCollection.insertOne(ticket)
+        res.send(result)
+    } catch (error) {
+        res.status(500).send({ error: error.message })
+    }
+})
+
+// Getting Ticket
+app.get('/api/tickets', verifyDbReady, async (req, res) => {
+    try {
+        const query = {}
+        if (req.query.vendorId) query.vendorId = req.query.vendorId;
+        const result = await ticketCollection.find(query).toArray();
+        res.send(result)
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+})
 
 
 
